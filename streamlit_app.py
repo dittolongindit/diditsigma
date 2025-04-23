38252f93c146240abedd78ab3f3e31a97c7f0112
dittolongindit <raditradithya503@gmail.com>
2025-04-23 14:55:23 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("DIDITMAXWIN353🔥⚡⚡")
+st.title("AditTolonginDit")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "stop judi, judi membuat mu rugi 🔥."
 )

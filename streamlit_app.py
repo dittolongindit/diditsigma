da63a105617dfb181d5361f0696882bffff06a5f
dittolongindit <raditradithya503@gmail.com>
2025-05-21 15:04:41 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,14 @@ import streamlit as st
 
 st.title("AditTolonginDit")
 st.write("stop judi, judi membuat mu rugi 🔥.")
-
-
 st.image("Screenshot_20250520_165832.jpg", width=200) 
+
+st.tiltle("Aplikasi Radeung Bisa Berhitung") 
+st.header("Radeung Bisa Ngitung, Coba Aja") 
+angka = st.number_input ("coba tulis angka apa aja, radeung mah pinter:", value=0, step=1) 
+
+if (angka % 2) == 0:
+    st.write(f"{angka} Kata Radeung itu adalah bilangan genap") 
+else:
+    st.write(f"{angka} Kata Radeung itu adalah bilangan ganjil") 
+    

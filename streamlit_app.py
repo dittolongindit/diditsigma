adb55ecfe8015c14fdaa8936371679c6060e0540
dittolongindit <raditradithya503@gmail.com>
2025-05-21 14:41:47 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,5 @@ import streamlit as st
 st.title("AditTolonginDit")
 st.write("stop judi, judi membuat mu rugi 🔥.")
 
-col1, col2, col3 = st.colums(3) 
 
-with col1
-    st.header("Kucing") 
-    st.image("https://static.streamlit.example/cat.jpg") 
+st.image("Screenshot_20250520_165832.jpg", width=200) 
